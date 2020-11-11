8724a71e8c9ee21d834eb944be837bf57ba73937
praveen3073 <pop3073@gmail.com>
2020-11-11 22:18:29 +0530

    Added: Total Employee Wage For A Month Using For Loop

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,6 +5,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch(empCheck) {
@@ -17,7 +18,10 @@ function getWorkingHours(empCheck) {
     }    
 }
 
-let empCheck = Math.floor(Math.random() * 100) % 3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Hours: " + empHrs + ", Employee Wage: " + empWage);
+let totalEmpHrs = 0;
+for(day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 100) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("Total Hours: " + totalEmpHrs + ", Employee Wage: " + empWage);
