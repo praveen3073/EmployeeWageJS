ef34dd61423c91d23d431021478f401dcaae494e
praveen3073 <pop3073@gmail.com>
2020-11-11 20:48:40 +0530

    Added: Print Employee Present Or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,8 @@
 console.log("Welcome to Employee Wage Simulation")
+
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 100) % 2;
+if(empCheck == IS_ABSENT)
+    console.log("Employee is absent");
+else
+    console.log("Employee is present");
