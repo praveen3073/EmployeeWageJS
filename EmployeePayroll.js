2fe0e4e2a975ece1fa60609cea343188f69d9273
praveen3073 <pop3073@gmail.com>
2020-11-15 12:16:39 +0530

    Added: Gender & Start Date In Employee Payroll

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -2,12 +2,16 @@ class EmployeePayrollData {
     // properties
     id;
     salary;
+    gender;
+    startDate;
 
     //constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4]; 
     }
 
     get name() {
@@ -19,8 +23,14 @@ class EmployeePayrollData {
     }
 
     toString() {
-        return "id: " + this.id + ", name: " + this.name + ", salary: " + this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric'}
+        const empDate = this.startDate === undefined ? "undefined" 
+                            : this.startDate.toLocaleDateString("en-US", options);
+        return "id: " + this.id + ", name: " + this.name + ", salary: " + this.salary
+            +  ", gender: " + this.gender + ", startDate: " + empDate;
     }
 }
-let employeePayrollData = new EmployeePayrollData(1, "Koko", 20000);
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 20000);
 console.log(employeePayrollData.toString());
+let employeePayrollData2 = new EmployeePayrollData(2, "Teresa", 50000, "F", new Date());
+console.log(employeePayrollData2.toString());
